23f8bf7b53c45d21d450f7b80265c85c4fd8cc9c
paulosedassari <pssantos.ph200@gmail.com>
2022-01-10 23:37:53 -0300

    Melhoria Class UI

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,7 @@
 package chess;
 
 import boadgame.Board;
-import boadgame.Position;
+import chess_pieces.King;
 import chess_pieces.Rook;
 
 public class ChessMatch {
@@ -24,14 +24,25 @@ public class ChessMatch {
 		}
 		return matriz;
 	}
-	
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
+
 	private void initialSetup() {
-		placeNewPiece('a', 1, new Rook(board, Color.Black));
-		placeNewPiece('h', 8, new Rook(board, Color.Black));
+		placeNewPiece('c', 1, new Rook(board, Color.White));
+		placeNewPiece('c', 2, new Rook(board, Color.White));
+		placeNewPiece('d', 2, new Rook(board, Color.White));
+		placeNewPiece('e', 2, new Rook(board, Color.White));
+		placeNewPiece('e', 1, new Rook(board, Color.White));
+		placeNewPiece('d', 1, new King(board, Color.White));
+		
+		placeNewPiece('c', 7, new Rook(board, Color.Black));
+		placeNewPiece('c', 8, new Rook(board, Color.Black));
+		placeNewPiece('d', 7, new Rook(board, Color.Black));
+		placeNewPiece('e', 7, new Rook(board, Color.Black));
+		placeNewPiece('e', 8, new Rook(board, Color.Black));
+		placeNewPiece('d', 8, new King(board, Color.Black));
 	}
 
 }
